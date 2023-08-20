95555170d16a01871c7250ae8b42642efcca0a6c
marimotona <ssmmo.06021224@gmail.com>
2023-08-20 21:00:58 +0900

    step10 : multi leter local variables and function

--- memo/README.md
+++ memo/README.md
@@ -112,3 +112,116 @@ ND_EXPR_STMTノードは式文を表すようにしている
    これが、stmt関数を通じて行われる。
 2. stmt関数は、トークンをexpr_stmt関数に渡し、式を解析
    その結果をND_EXPR_STMTノードにラップして返す
+
+
+
+### step10
+
+#### parse.c
+
+##### new_var_node関数
+
+変更前の実装
+```c
+static Node *new_var_node(char name) {
+    Node *node = new_node(ND_VAR);
+    node->name = name;
+    return node;
+}
+```
+単一の文字(char型)を受けとって、それをnode->nameフィールドに設定
+
+変更後の実装
+```c
+static Node *new_var_node(Obj *var) {
+    Node *node = new_node(ND_VAR);
+    node->var = var;
+    return node;
+}
+```
+Obj型のポインタを使用することで、複数の文字からなる変数をサポートしている
+
+
+##### primary関数
+
+```c
+if (tok->kind == TK_IDENT) {
+    Obj *var = find_var(tok);
+    if(!var)
+        var = new_lvar(strndup(tok->loc, tok->len)); 
+    *rest = tok->next;
+    return new_var_node(var);
+}
+```
+strndup(*tok->loc)
+トークンの位置情報から変数名をコピー
+new_lvar関数に渡す
+
+```c
+strndup(tok->loc, tok->len)
+```
+指定された文字列から指定された文字列の長さの部分文字列をコピーし、
+新しいメモリ領域に格納し、そのポインタを返す
+
+トークンが表している変数名が新しいメモリ領域に格納されている
+
+
+最後にreturn new_var_node(var);
+で、ASTノードを作成しノードを返す
+
+#### tokenize.c
+
+##### tokenize関数
+
+```c
+if(is_ident1(*p)) {
+    char *start = p;
+    do {
+        p++;
+    } while (is_ident2(*p));
+    cur = cur->next = new_token(TK_IDENT, start, p);
+    continue;
+}
+```
+
+is_ident1で、最初の文字が有効な文字か確認
+
+```c
+static bool is_ident1(char c) {
+    return ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || c == '_';
+}
+```
+
+有効な場合は、開始位置を保持しておく
+2文字目以降も、条件に合っているか確認
+
+```c
+cur = cur->next = new_token(TK_IDENT, start, p);
+```
+終点を見つけたら、startから、p（終わり）までの文字列をトークナイズする
+
+
+##### Function
+
+Node型はASTの一部を表すのに対して、Function型は完全な関数を表現する
+Functuionという一つの大きなもので、今まで作成した情報をラップしている
+
+parse.cの中で、Function型のオブジェクトを作成している
+
+```c
+Function *parse(Token *tok) {
+    Node head;
+    Node *cur = &head;
+    while (tok->kind != TK_EOF)
+      cur = cur->next = stmt(&tok, tok);
+    Function *prog = calloc(1, sizeof(Function));
+    prog->body = head.next;
+    prog->locals = locals;
+    return prog;
+}
+```
+parse関数の最後の部分で、
+Function型のオブジェクトとして、
+・関数の本体
+・ローカル変数、スタックのサイズ
+を保持したオブジェクトを作成している
--- parse.c
+++ parse.c
@@ -1,5 +1,7 @@
 #include "9cc.h"
 
+Obj *locals;
+
 static Node *expr(Token **rest, Token *tok);
 static Node *expr_stmt(Token **rest, Token *tok);
 static Node *assign(Token **rest, Token *tok);
@@ -9,6 +11,14 @@ static Node *add(Token **rest, Token *tok);
 static Node *mul(Token **rest, Token *tok);
 static Node *unary(Token **rest, Token *tok);
 static Node *primary(Token **rest, Token *tok);
+
+
+static Obj *find_var(Token *tok) {
+    for (Obj *var = locals; var; var = var->next)
+      if (strlen(var->name) == tok->len && !strncmp(tok->loc, var->name, tok->len))
+        return var;
+    return NULL;
+}
 
 static Node *new_node(NodeKind kind) {
   Node *node = calloc(1, sizeof(Node));
@@ -35,16 +45,26 @@ static Node *new_num(int val) {
   return node;
 }
 
-static Node *new_var_node(char name) {
+
+static Node *new_var_node(Obj *var) {
     Node *node = new_node(ND_VAR);
-    node->name = name;
+    node->var = var;
     return node;
 }
+
+static Obj *new_lvar(char *name) {
+    Obj *var = calloc(1, sizeof(Obj));
+    var->name = name;
+    var->next = locals;
+    locals = var;
+    return var;
+}
+
 
 static Node *stmt(Token **rest, Token *tok) {
   return expr_stmt(rest, tok);
 }
-// expr-stmt = expr ";"
+
 static Node *expr_stmt(Token **rest, Token *tok) {
   Node *node = new_unary(ND_EXPR_STMT, expr(&tok, tok));
   *rest = skip(tok, ";");
@@ -169,9 +189,11 @@ static Node *primary(Token **rest, Token *tok) {
   }
 
   if (tok->kind == TK_IDENT) {
-    Node *node = new_var_node(*tok->loc);
+    Obj *var = find_var(tok);
+    if(!var)
+      var = new_lvar(strndup(tok->loc, tok->len));
     *rest = tok->next;
-    return node;
+    return new_var_node(var);
   }
 
   if (tok->kind == TK_NUM) {
@@ -183,10 +205,13 @@ static Node *primary(Token **rest, Token *tok) {
   error_tok(tok, "expected an expression");
 }
 
-Node *parse(Token *tok) {
+Function *parse(Token *tok) {
     Node head;
     Node *cur = &head;
     while (tok->kind != TK_EOF)
       cur = cur->next = stmt(&tok, tok);
-    return head.next;
+    Function *prog = calloc(1, sizeof(Function));
+    prog->body = head.next;
+    prog->locals = locals;
+    return prog;
 }
